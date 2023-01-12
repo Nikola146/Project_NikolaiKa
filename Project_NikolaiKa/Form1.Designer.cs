f58f0c61d36a1c0ba03cf1597006dbe9b6e0d1e0
Nikola146 <kolya146@gmail.com>
2023-01-12 19:19:55 +0200

    Добавьте файлы проекта.

---
+++ Project_NikolaiKa/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Project_NikolaiKa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.gender_obj = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.smoke_choose = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.age_obj = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.age_obj)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show life expectancy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gender_obj
+            // 
+            this.gender_obj.AutoSize = true;
+            this.gender_obj.Checked = true;
+            this.gender_obj.Location = new System.Drawing.Point(0, 19);
+            this.gender_obj.Name = "gender_obj";
+            this.gender_obj.Size = new System.Drawing.Size(46, 17);
+            this.gender_obj.TabIndex = 3;
+            this.gender_obj.TabStop = true;
+            this.gender_obj.Text = "Man";
+            this.gender_obj.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(64, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Woman";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.gender_obj);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(162, 172);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(127, 46);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose gender";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.smoke_choose);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(488, 172);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(142, 46);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Do you smoke?";
+            // 
+            // smoke_choose
+            // 
+            this.smoke_choose.AutoSize = true;
+            this.smoke_choose.Location = new System.Drawing.Point(0, 19);
+            this.smoke_choose.Name = "smoke_choose";
+            this.smoke_choose.Size = new System.Drawing.Size(43, 17);
+            this.smoke_choose.TabIndex = 3;
+            this.smoke_choose.Text = "Yes";
+            this.smoke_choose.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(91, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(39, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "No";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(598, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "This program will show your approximate life expectancy in Estonia, depending on " +
+    "your gender and whether you smoke or not.";
+            // 
+            // age_obj
+            // 
+            this.age_obj.Location = new System.Drawing.Point(324, 188);
+            this.age_obj.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.age_obj.Name = "age_obj";
+            this.age_obj.Size = new System.Drawing.Size(120, 20);
+            this.age_obj.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Your age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.age_obj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.age_obj)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton gender_obj;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton smoke_choose;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown age_obj;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project_NikolaiKa/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_NikolaiKa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal result = calculate(age_obj.Value, smoke_choose.Checked, gender_obj.Checked);
+            MessageBox.Show("You will live for another "+result+" years");
+        }
+
+        private decimal calculate(decimal age, bool smoke, bool gender)
+        {
+            int averageLifeExpectancy = 0;
+            if (gender)
+            {
+                averageLifeExpectancy = 76;
+            }
+            else
+            {
+                averageLifeExpectancy = 82;
+            }
+
+            if (smoke)
+            {
+                if (gender)
+                {
+                    averageLifeExpectancy -= 12;
+                }
+                else
+                {
+                    averageLifeExpectancy -= 11;
+                }
+            }
+
+            decimal yearsLeft = averageLifeExpectancy - age;
+            return yearsLeft;
+        }
+    }
+}
